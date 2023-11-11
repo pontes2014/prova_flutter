1b5233bd6a9386d7c5864235de3306ff479c93ae
Pedro Pontes <91500779+pontes2014@users.noreply.github.com>
2023-11-11 13:21:16 -0300

    att

--- lib/services/api_services.dart
+++ lib/services/api_services.dart
@@ -2,21 +2,19 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class MockApiService {
-  
   static Future<List<Map<String, dynamic>>> getMockUsers() async {
     final mockUsers = [
-      {'id': 1, 'name': 'John Doe', 'email': 'pontes', 'password': 'pontes'},
-      {'id': 2, 'name': 'rogerio', 'email': 'admin', 'password': 'admin'},
-      {'id': 3, 'name': 'roberto', 'email': 'roberto', 'password': '123'},
-      {'id': 4, 'name': 'Smith', 'email': 'admin', 'password': '123'},
-      {'id': 5, 'name': 'robert', 'email': 'teste', 'password': 'teste'},
+      {'id': 1, 'email': 'pontes', 'password': 'pontes'},
+      {'id': 2, 'email': 'admin', 'password': 'admin'},
+      {'id': 3, 'email': 'roberto', 'password': '123'},
+      {'id': 4, 'email': 'admin', 'password': '123'},
+      {'id': 5, 'email': 'teste', 'password': 'teste'},
     ];
 
     final jsonData = jsonEncode(mockUsers);
 
     final mockResponse = http.Response(jsonData, 200);
 
-    
     return mockUsers;
   }
 }
